dc7ff367993aab3b57160582ff904e541b4b59b6
Diana Hossain Arif <25801886+diabotez@users.noreply.github.com>
2026-03-19 11:37:40 +0100

    update index.js and readme and remove generated files

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const core = require('./@actions/core');
+import * as core from "@actions/core";
+import * as github from "@actions/github";
 
 try {
-    const name = core.getInput('name');
-    const greeting = `Hello, ${name}! Welcome to Barum.`;
-    core.setOutput('greeting', greeting);
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput("who-to-greet");
+  core.info(`Hello ${nameToGreet}!`);
+
+  // Get the current time and set it as an output variable
+  const time = new Date().toTimeString();
+  core.setOutput("time", time);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  core.info(`The event payload: ${payload}`);
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
