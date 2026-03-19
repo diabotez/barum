6cca31fe16c16111fcfe83e6841c363b84318dd8
Diana Hossain Arif <25801886+diabotez@users.noreply.github.com>
2026-03-19 10:56:05 +0100

    push new attempt to fix npm error

---
+++ dist/index.cjs
@@ -0,0 +1,16 @@
+/******/ /* webpack/runtime/compat */
+/******/ 
+/******/ if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = new URL('.', import.meta.url).pathname.slice(import.meta.url.match(/^file:\/\/\/\w:/) ? 1 : 0, -1) + "/";
+/******/ 
+/************************************************************************/
+var __webpack_exports__ = {};
+const core = require('@actions/core');
+
+try {
+    const name = core.getInput('name');
+    const greeting = `Hello, ${name}! Welcome to Barum.`;
+    core.setOutput('greeting', greeting);
+} catch (error) {
+    core.setFailed(error.message);
+}
+
